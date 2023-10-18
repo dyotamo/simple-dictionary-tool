be87a593f8f997b76fde35aef20970a8ba5cb6c1
Dassone J. Jotamo <dassone.jotamo@vm.co.mz>
2023-10-18 15:38:23 +0200

    refactor: convert to python3.

--- dictionary.py
+++ dictionary.py
@@ -1,59 +1,41 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import print_function
-
 import sys
 import utils
 
 args = sys.argv[1:]
 
 if utils.dssize(args) == 0:
-    print('Usage: dictionary [key] | dictionary --help | dictionary --version')
+    print("Usage: dictionary [key] | dictionary --help")
+    exit(1)
 else:
-    key = ' '.join(args)
-
-    if key == '--version':
-        version = '''dictionary (<<OS>>) 1.0
-Copyright (C) 2015 Dássone Yotamo - ddd
-This is free software; see the source for copying conditions.  There is NO
-warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.'''
-
-        print(version)
-        exit()
-
+    key = " ".join(args)
     if key == '--help':
-        help = '''Under construction.'''
+        print("Usage: dictionary [key] | dictionary --help")
+        exit(0)
 
-        print(help)
-        exit()
+    with open("dic.cd", "r") as fd:
+        fflag = False
 
-    fd = open('dic.cd', 'r')
-
-    fflag =  False
-
-    for x in fd:
-       temp_str = x[:-1]
-
-       if temp_str.lower() == key.lower(): # [:-1] because this string in \n-ended. lower() for being icase ...
-            fflag = True
-            print('Result found for: %s' % (temp_str,))
-            break
-
-    if not fflag:
-        print('Results not found for: %s' % (key))
-        fd.seek(0)
-
-        c = 0
-
-        for y in utils.get_keys(fd):
-            if utils.analyze_str(key.lower(), y.lower()):
-                print('Did you mean: %s?' % (y,))
-    else:
         for x in fd:
-            if x[0] == '\t' or x == '\n':
-                print(x, end='')
-            else:
+            temp_str = x[:-1]
+
+            # [:-1] because this string in \n-ended. lower() for being icase ...
+            if temp_str.lower() == key.lower():
+                fflag = True
+                print("Result found for:", temp_str)
                 break
 
-    fd.close()
+        if not fflag:
+            print("Results not found for:", key)
+            fd.seek(0)
 
+            c = 0
+
+            for y in utils.get_keys(fd):
+                if utils.analyze_str(key.lower(), y.lower()):
+                    print("Did you mean: ", y)
+        else:
+            for x in fd:
+                if x[0] == "\t" or x == "\n":
+                    print(x, end='')
+                else:
+                    break
--- utils.py
+++ utils.py
@@ -1,32 +1,36 @@
 def dssize(arg):
-    '''Compute the size of a data structure,
-I didn't see this in Python'''
+    """Compute the size of a data structure,
+I didn't see this in Python"""
 
-    temp = 0;
+    temp = 0
 
-    for x in arg:
+    for _ in arg:
         temp += 1
 
     return temp
 
+
 def get_keys(arg):
-    '''Returns a list of keys in the dictionary (Not working)'''
+    """Returns a list of keys in the dictionary (Not working)"""
     temp = []
     for x in arg:
-        if x[0] != '\t' and x != '\n':
+        if x[0] != "\t" and x != "\n":
             temp.append(x[:-1])
 
     return temp
 
+
 def generate_str(arg):
-    temp = ''
-    for x in range(arg):
-        temp += ' '
+    temp = ""
+    for _ in range(arg):
+        temp += " "
 
     return temp
 
+
 def probability(arg0, arg1):
     return float(arg0)/arg1
+
 
 def analyze_str(arg0, arg1):
     s1 = dssize(arg0)
@@ -39,14 +43,9 @@ def analyze_str(arg0, arg1):
         str2 += generate_str(s1 - s2)
     elif s1 < s2:
         str1 += generate_str(s2 - s1)
-    else:
-        pass
 
-    if probability(match_nr(str1, str2), dssize(str1)) >= 0.5: # Or str2, same size ...
-        return True
-    else:
-        return False
-    
+    # Or str2, same size ...
+    return probability(match_nr(str1, str2), dssize(str1)) >= 0.5
 
 
 def match_nr(arg0, arg1):
